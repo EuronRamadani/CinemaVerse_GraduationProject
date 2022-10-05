0a49cabfe1d4071361e5cb318b629802866debd6
EuronRamadani <euronra@gmail.com>
2022-10-05 17:21:54 +0200

    Implemented Event Photos in adminDashboard

--- client-side/src/router/routes/event.js
+++ client-side/src/router/routes/event.js
@@ -3,44 +3,66 @@ export default [
 		path: "/events",
 		name: "Events",
 		meta: {
-		  layout: "public",
+			layout: "public",
 		},
 		component: () =>
-		  import(
-			/* webpackChunkName: "register" */ "../../views/Events/Events.vue"
-		  ),
-	  },
+			import(
+				/* webpackChunkName: "register" */ "../../views/Events/Events.vue"
+			),
+	},
 	{
 		path: "/event",
 		name: "Event",
 		component: () =>
-			import(
-				/* webpackChunkName: "register" */ "../../views/Events/Event.vue"
-			),
+			import(/* webpackChunkName: "register" */ "../../views/Events/Event.vue"),
 	},
 	{
 		path: "/admin/events/cinemaId/:cinemaId/create",
 		name: "event-create",
 		meta: {
-		  requiresAuth: true,
-		  layout: "dashboard",
+			requiresAuth: true,
+			layout: "dashboard",
 		},
 		component: () =>
-		  import(
-			/* webpackChunkName: "create-event" */ "../../views/Admin/Events/CreateEvent.vue"
-		  ),
-	  },
-	  {
+			import(
+				/* webpackChunkName: "create-event" */ "../../views/Admin/Events/CreateEvent.vue"
+			),
+	},
+	{
 		path: "/admin/events/edit/:eventId",
 		name: "event-edit",
 		meta: {
-		  requiresAuth: true,
-		  layout: "dashboard",
+			requiresAuth: true,
+			layout: "dashboard",
 		},
 		component: () =>
-		  import(
-			/* webpackChunkName: "cinema-details" */ "../../views/Admin/Events/EditEvent.vue"
-		  ),
-	  },
-	
+			import(
+				/* webpackChunkName: "cinema-details" */ "../../views/Admin/Events/EditEvent.vue"
+			),
+	},
+
+	{
+		path: "/admin/events/cinemaId/:cinemaId/:eventId",
+		name: "event-details",
+		meta: {
+			requiresAuth: true,
+			layout: "dashboard",
+		},
+		component: () =>
+			import(
+				/* webpackChunkName: "movie-details" */ "../../views/Admin/Events/EventDetails.vue"
+			),
+	},
+	{
+		path: "/admin/events/cinemaId/:cinemaId/add-photo/:eventId",
+		name: "event-add-photo",
+		meta: {
+			requiresAuth: true,
+			layout: "dashboard",
+		},
+		component: () =>
+			import(
+				/* webpackChunkName: "cinema-details" */ "../../views/Admin/Events/AddEventPhoto.vue"
+			),
+	},
 ];
